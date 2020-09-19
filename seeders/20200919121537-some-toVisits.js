ad22203f018a41f3ad2090252c35b72b842f8eaf
nmegrant <nme.grant@gmail.com>
2020-09-19 14:18:54 +0200

    Seeds for tovisits

--- seeders/20200919121537-some-toVisits.js
+++ seeders/20200919121537-some-toVisits.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert(
-      "visiteds",
+      "toVisits",
       [
         {
           city: "Rome",
