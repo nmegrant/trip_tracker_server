53c4d9dfc9ee1f0be55370871fdac036ba84d4a0
nmegrant <nme.grant@gmail.com>
2020-09-19 14:18:42 +0200

    Seeds for tovisit

---
+++ seeders/20200919121537-some-toVisits.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "visiteds",
+      [
+        {
+          city: "Rome",
+          country: "Italy",
+          long: 12.4964,
+          lat: 41.9028,
+        },
+        {
+          city: "Athens",
+          country: "Greece",
+          long: 12.4964,
+          lat: 41.9028,
+        },
+        {
+          city: "Copenhagen",
+          country: "Denmark",
+          long: 23.7275,
+          lat: 37.9838,
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("toVisits", null, {});
+  },
+};
