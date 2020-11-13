030a2ad3961dc911cd33a75911c2ecf4987e0e0d
nmegrant <nme.grant@gmail.com>
2020-11-13 18:13:41 +0100

    Set up signup with bcrypt

--- routers/user.js
+++ routers/user.js
@@ -1,4 +1,6 @@
 const { Router } = require("express");
+const User = require("../models").user;
+const bcrypt = require("bcrypt");
 
 const router = new Router();
 
@@ -12,9 +14,28 @@ router.post("/login", async (request, response, next) => {
 
 router.post("/signup", async (request, response, next) => {
   try {
-    console.log(request.body);
-  } catch {
-    console.log(`The signup error is: ${error}`);
+    const { firstName, lastName, email, password, country } = request.body;
+    if (!email || !password || !firstName || !lastName) {
+      return response
+        .status(400)
+        .send({ message: "Please supply the required information." });
+    }
+    const newUser = await User.create({
+      email,
+      password: bcrypt.hashSync(password, 10),
+      firstName,
+      lastName,
+      country,
+    });
+    delete newUser.dataValues["password"];
+    response.status(201).send({ token, ...newUser.dataValues });
+  } catch (error) {
+    if (error.name === "SequelizeUniqueConstraintError") {
+      return response
+        .status(400)
+        .send({ message: "There is an existing account with this email." });
+    }
+    return response.status(400).send({ message: "Something went wrong" });
   }
 });
 
