97fd96cf555696bfac957c1a0aa408e202cd493c
nmegrant <nme.grant@gmail.com>
2020-11-22 10:46:42 +0100

    Add db relations

--- models/usertovisit.js
+++ models/usertovisit.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class userToVisit extends Model {
     /**
@@ -10,17 +8,20 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      userToVisit.belongsTo(models.user);
     }
-  };
-  userToVisit.init({
-    country: DataTypes.STRING,
-    city: DataTypes.STRING,
-    date: DataTypes.DATE,
-    days: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'userToVisit',
-  });
+  }
+  userToVisit.init(
+    {
+      country: DataTypes.STRING,
+      city: DataTypes.STRING,
+      date: DataTypes.DATE,
+      days: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "userToVisit",
+    }
+  );
   return userToVisit;
 };
--- models/uservisited.js
+++ models/uservisited.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class userVisited extends Model {
     /**
@@ -10,17 +8,20 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      userVisited.belongsTo(models.user);
     }
-  };
-  userVisited.init({
-    country: DataTypes.STRING,
-    city: DataTypes.STRING,
-    date: DataTypes.DATE,
-    days: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'userVisited',
-  });
+  }
+  userVisited.init(
+    {
+      country: DataTypes.STRING,
+      city: DataTypes.STRING,
+      date: DataTypes.DATE,
+      days: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "userVisited",
+    }
+  );
   return userVisited;
 };
